b3b0fc11ad71700a39d7df52391d8b9e3478f58b
unknown <mcbride.mik@gmail.com>
2019-10-16 21:31:48 -0400

    ITS WORKING

---
+++ MavenBase/src/main/java/com/revature/tuition/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.revature.tuition.service;
+
+import com.revature.tuition.pojo.Employee;
+
+public interface EmployeeService {
+	public Employee loginEmployee(String username, String password);
+}
---
+++ MavenBase/src/main/java/com/revature/tuition/service/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.revature.tuition.service;
+
+import com.revature.tuition.pojo.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public Employee loginEmployee(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
