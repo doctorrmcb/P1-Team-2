8915ec246d1b419eeaccf2391de90668a98daa0b
doriapameni <doriapameni@gmail.com>
2019-10-19 00:53:57 -0400

    added javascript code to populate n notification message depending on n argument value

--- CentralMenuEmployee/JS/code.js
+++ CentralMenuEmployee/JS/code.js
@@ -6,8 +6,12 @@ function goToViewManageFromMenu() {
     location.replace(href = 'ViewEditDeleteReimbursementForm/view_manage_reimbursement.html');
 };
 
-function goTogUpdateForm() {
+function goToUpdateForm() {
     location.replace(href = '../UpdatePendingReimbursementForm/Update_Pending_Form.html');
+};
+
+function goToViewForm() {
+    location.replace(href = '../ViewEditDeleteReimbursementForm/view_manage_reimbursement.html');
 };
 
 function LogoutFromMenu() {
@@ -25,4 +29,42 @@ function goFromPageToMenu() {
 function deleteRow(r) {
     let i = r.parentNode.parentNode.rowIndex;
     document.getElementById("myTable").deleteRow(i);
+};
+
+function submittedForm() {
+    //alert("The reimbursement form was successfully submitted");
+    $('.toast').toast('show')
+        //$('.toast').toast(option)
+    goToUpdateForm()
+};
+
+
+function dateDisplay() {
+    var x = document.getElementById("dob").value;
+    document.getElementById("demo").innerHTML = x;
+};
+
+function openNav() {
+    notification(4);
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+};
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+};
+
+function notification(n) {
+    for (let i = 0; i < n; i++) {
+        createAnchor();
+    }
+
+};
+
+
+function createAnchor() {
+    var para = document.createElement("P");
+    para.innerHTML = "message";
+    document.getElementById("myCreation").appendChild(para);
 }
