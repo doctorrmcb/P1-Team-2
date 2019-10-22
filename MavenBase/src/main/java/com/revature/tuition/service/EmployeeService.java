b2295257580e3f5669025e138bc5835c74ecdba8
unknown <mcbride.mik@gmail.com>
2019-10-21 22:30:01 -0400

    Removed merge conflicts

--- MavenBase/src/main/java/com/revature/tuition/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.revature.tuition.service;
-
-import com.revature.tuition.pojo.Employee;
-
-public interface EmployeeService {
-	public Employee loginEmployee(String username, String password);
-}
