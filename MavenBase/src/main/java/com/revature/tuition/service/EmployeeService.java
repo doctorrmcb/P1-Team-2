2c7d06a6c6f2af63bca0ccd8fcf2c75a3d1f2367
unknown <mcbride.mik@gmail.com>
2019-10-21 20:37:21 -0400

    Finished registration functionality (Should have input validation and testing added later, went with MVP)

--- MavenBase/src/main/java/com/revature/tuition/service/EmployeeService.java
+++ MavenBase/src/main/java/com/revature/tuition/service/EmployeeService.java
@@ -4,4 +4,5 @@ import com.revature.tuition.pojo.Employee;
 
 public interface EmployeeService {
 	public Employee loginEmployee(String username, String password);
+	public Employee registerEmployee(String name, int employeeId, String address, String email, String phone, String reportsToStr, String title, String username, String password);
 }
